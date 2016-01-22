acd02c95383892811aadb592aa47064ff648769c
Charles Mailly <charles.mailly@free.fr>
2016-01-22 11:34:24 +0100

    Ajout des étapes de la génération du labyrinthe

--- Code/Algo2.c
+++ Code/Algo2.c
@@ -1,13 +1,18 @@
 #include "Utils.h"
 
+Maze* mAlgo2Maze = NULL;
+int mAlgo2Step = 0;
+
 int ALGO2_create(SDL_Renderer* renderer)
 {
+    mAlgo2Maze = createMaze(TILE_WIDTH,TILE_HEIGHT);
 
+    // TODO : error
 }
 
 void ALGO2_destroy()
 {
-
+    destroyMaze(mAlgo2Maze);
 }
 
 void ALGO2_handleEvent(SDL_Event event, SDL_Context* context)
@@ -16,6 +21,21 @@ void ALGO2_handleEvent(SDL_Event event, SDL_Context* context)
     if (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
     {
         STATES_switch(STATE_MENU,context);
+    }
+    if (event.type == SDL_MOUSEBUTTONDOWN && event.button.state == SDL_PRESSED && event.button.button == SDL_BUTTON_LEFT)
+    {
+        mAlgo2Step++;
+        switch (mAlgo2Step)
+        {
+            case 1: generationStep1(mAlgo2Maze); break;
+            case 2: generationStep2(mAlgo2Maze); break;
+            case 3: generationStep3(mAlgo2Maze); break;
+            case 4: generationStep4(mAlgo2Maze); break;
+            case 5: generationStep5(mAlgo2Maze); break;
+            case 6: generationStep6(mAlgo2Maze); break;
+            case 7: generationStep7(mAlgo2Maze); break;
+            default: break;
+        }
     }
 }
 
@@ -26,5 +46,5 @@ void ALGO2_update()
 
 void ALGO2_render(SDL_Renderer* renderer)
 {
-
+    renderMaze(renderer, mAlgo2Maze);
 }
