e5333d3daada37104e55b3c85c633535008c7b50
Charles Mailly <charles.mailly@free.fr>
2016-03-25 09:47:28 +0100

    Update Path.h

--- Code/Path.h
+++ Code/Path.h
@@ -6,11 +6,13 @@
 void generateMouseTree(Maze* maze, int mouseIndex);
 
 void moveToTarget(Mouse* mouse);
-/*void moveToMouse(Cat* cat)*/
+/*void moveToMouse(Cat* cat);*/
 void findDest(Maze* maze, Mouse* mouse);
 
 void nextMoveMouse(Maze* maze, int mouseIndex);
 void nextMoveCat(Maze* maze, int catIndex);
 
+void RemoveCheeseFromMaze(Maze* maze, int cheeseIndex);
+void RemoveMouseFromMaze(Maze* maze, int mouseIndex);
 
 #endif // PATH_H_INCLUDED
